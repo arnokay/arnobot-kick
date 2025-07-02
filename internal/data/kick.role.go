8355bf1cf00b20f69870eca0a3f966c9ed55aff9
arnokay <arnokay.work@gmail.com>
2025-07-03 01:04:59 +0300

    feat: add check if higher role already is set

--- internal/data/kick.role.go
+++ internal/data/kick.role.go
@@ -8,16 +8,16 @@ import (
 func GetChatterRole(badges []gokick.Badge) data.ChatterRole {
 	var role = data.ChatterPleb
 	for _, badge := range badges {
-		if badge.Type == "subscriber" {
+		if badge.Type == "subscriber" && role < data.ChatterSub {
 			role = data.ChatterSub
 		}
-		if badge.Type == "og" || badge.Type == "vip" {
+		if (badge.Type == "og" || badge.Type == "vip") && role < data.ChatterVIP {
 			role = data.ChatterVIP
 		}
-		if badge.Type == "moderator" {
+		if badge.Type == "moderator" && role < data.ChatterModerator {
 			role = data.ChatterModerator
 		}
-		if badge.Type == "broadcaster" {
+		if badge.Type == "broadcaster" && role < data.ChatterBroadcaster {
 			role = data.ChatterBroadcaster
 		}
 	}
